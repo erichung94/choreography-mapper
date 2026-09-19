0c3f025d074bd7d457c81d883016e6dc1aacd327
erichung94 <47572206+erichung94@users.noreply.github.com>
2026-09-19 13:34:26 -0500

    Break a test on purpose to see if CI fails

--- src/domain/ticks.test.ts
+++ src/domain/ticks.test.ts
@@ -3,7 +3,7 @@ import { isCountTick } from './ticks'
 
 describe('isCountTick', () => {
   it('is true on a count and false on a &', () => {
-    expect(isCountTick(0)).toBe(true) //count 1
+    expect(isCountTick(0)).toBe(false) //count 1
     expect(isCountTick(1)).toBe(false) //count 1.5
     expect(isCountTick(2)).toBe(true) //count 2
   })
